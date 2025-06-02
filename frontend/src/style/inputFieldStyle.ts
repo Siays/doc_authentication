d00aa144a1a016ab52111f6825127f96c988e5e7
Siays <sysheng1@hotmail.com>
2025-06-02 23:08:54 +0800

    new doc page
    create styling ts, new document validation

---
+++ frontend/src/style/inputFieldStyle.ts
@@ -0,0 +1,9 @@
+export const baseInputClass = 
+    "block w-full rounded-md border px-3 py-2 shadow-sm outline outline-1 bg-white outline-gray-300 focus:outline-indigo-600";
+
+export const unmodifiableInputClass = 
+    "mt-2 block w-full rounded-md border px-3 py-2 shadow-sm outline outline-1 bg-gray-100 text-gray-500 outline-gray-300 cursor-not-allowed";
+            
+export const errorInputClass = "outline-red-500 border-red-300";
+
+export const errorTextClass = "text-sm text-red-600 mt-1";
